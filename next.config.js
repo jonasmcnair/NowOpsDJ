c6427a0bdddaf69b3cc91e4a22851f3c1750d92c
jonasmcnair <129911148+jonasmcnair@users.noreply.github.com>
2026-02-21 19:54:15 +1100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  env: {
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
 }
 module.exports = nextConfig
